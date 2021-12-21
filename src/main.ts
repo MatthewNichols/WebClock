23200263fe5b0b01ccc6f863cda9607242b6673f
Matthew Nichols <matthew@matthew-nichols.com>
2021-12-21 16:42:04 +0100

    Added the beginning of differentiated styling based on time.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,41 @@ import './style.scss'
 const minuteHand = document.getElementById('minute-hand');
 const hourHand = document.getElementById('hour-hand');
 
+var lastHourNumber: number = -1;
+function adjustTickClasses(currHourNumber: number) {
+  if (lastHourNumber === currHourNumber) {
+    return;
+  }
+
+  const tickNumbers = [3, 6, 9, 12];
+  tickNumbers.forEach((tickNum) => {
+    const tickElem = document.getElementById(`tick-${tickNum}`) as HTMLElement;
+    //something here about dealing with 12
+    const diff = Math.abs((tickNum % 12) - (currHourNumber % 12));
+    
+    let currClass: string | null = null;
+    tickElem.classList.forEach((c) => currClass = c.startsWith("distance") ? c : currClass);
+    if (currClass) {
+      tickElem.classList.replace(currClass, `distance-${diff}`);
+    } else {
+      tickElem.classList.add(`distance-${diff}`);
+    }
+
+    console.log(currHourNumber, tickNum, diff)
+  })
+  
+
+
+  lastHourNumber = currHourNumber;
+}
+
 setInterval(() => {
   const nowValue = new Date();
   const minutesPosition = (nowValue.getMinutes() + (nowValue.getSeconds() / 60)) * 6;
   const hourPos = ((nowValue.getHours() % 12) + (nowValue.getMinutes() / 60)) * 30;
   
   minuteHand?.setAttribute("transform", `rotate(${ minutesPosition }, 300, 300)`);
-  hourHand?.setAttribute("transform", `rotate(${ hourPos}, 300, 300)`)  
+  hourHand?.setAttribute("transform", `rotate(${ hourPos}, 300, 300)`);
+
+  adjustTickClasses(nowValue.getHours());
 }, 1000);
